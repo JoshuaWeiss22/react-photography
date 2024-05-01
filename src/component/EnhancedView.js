4aa7c011a598be66c6c22722efc092b9b7180306
Joshua Weiss <97697115+JoshuaWeiss22@users.noreply.github.com>
2024-05-01 13:59:25 -0700

    first change

--- src/component/EnhancedView.js
+++ src/component/EnhancedView.js
@@ -21,4 +21,4 @@ export default EnhancedView;
 //Create a button, bottom right of photo that when I click I show all the attributes that I have about the photo in text
 //Create a copy url button for the photo url
 //Using react contact and reducers (https://react.dev/reference/react/useReducer)
-//Change airtable to make it have a more meaniingful sorting method
+//Change airtable to make it have a more meangit ingful sorting method
